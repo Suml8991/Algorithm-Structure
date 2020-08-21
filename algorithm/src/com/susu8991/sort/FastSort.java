52f2e66298084a8c52806faacb8ea46f8b2a1057
suminglei <suminglei@jd.com>
2020-08-21 17:22:55 +0800

    目录+数据结构实现+算法实现

---
+++ algorithm/src/com/susu8991/sort/FastSort.java
@@ -0,0 +1,11 @@
+package com.susu8991.sort;
+
+/**
+ * @Program: Algorithm-Structure
+ * @Description:
+ * @Author: suminglei
+ * @Date: 2020-08-21 17:20:45
+ */
+public class FastSort {
+
+}
---
+++ structure/src/com/susu8991/stack/Stack.java
@@ -0,0 +1,10 @@
+package com.susu8991.stack;
+
+/**
+ * @Program: Algorithm-Structure
+ * @Description:
+ * @Author: suminglei
+ * @Date: 2020-08-21 17:22:04
+ */
+public class Stack {
+}
