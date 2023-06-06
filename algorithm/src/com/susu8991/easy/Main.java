4189cdbb7d960f30149397d4e50d2f4e0e836327
suminglei <suminglei@jd.com>
2023-06-06 18:44:12 +0800

    commit

--- algorithm/src/com/susu8991/easy/Main.java
+++ algorithm/src/com/susu8991/easy/Main.java
@@ -3,5 +3,6 @@ package com.susu8991.easy;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
