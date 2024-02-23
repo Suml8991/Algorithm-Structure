c5f8bb8ac0eedcc8a5ff20041bf7583999d68b9a
suminglei <suminglei@jd.com>
2024-02-23 11:04:44 +0800

    添加二分查找

--- algorithm/src/com/susu8991/easy/Main.java
+++ algorithm/src/com/susu8991/easy/Main.java
@@ -1,8 +1,22 @@
 package com.susu8991.easy;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        System.out.println(new BigDecimal("8920.0").intValue());
+
+        String bindCardRiskPinList = "jd_5dd6759714c7a,jd_5dd6759714c7a";
+        List<String> pinArray = Arrays.stream(bindCardRiskPinList.split(",")).collect(Collectors.toList());
+        if(pinArray.contains("jd_5d6759714c7a")){
+            System.out.println("risk");
+
+        }
+        Integer a= null;
+        System.out.println(String.valueOf(a));
     }
 }
